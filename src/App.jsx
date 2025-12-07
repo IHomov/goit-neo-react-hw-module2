193f49c92cb4102873ec0a33b7566bdb831133bb
Inna Khomovska <innapariy@gmail.com>
2025-12-08 00:59:00 +0100

    Step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
 import Notification from "./components/Notification/Notification";
 
-
 import "./App.css";
 
 function App() {
-  
-  const [feedback, setFeedback] = useState({
-    good: 5,
-    neutral: 3,
-    bad: 2,
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
   });
-  const [hasFeedback, setHasFeedback] = useState(false);
 
-    const updateFeedback = (feedbackType) => {
-    setFeedback((prevFeedback) => {
-      const newFeedback = { 
-        ...prevFeedback, 
-        [feedbackType]: prevFeedback[feedbackType] + 1 
-      };
-      if (newFeedback.good > 0 || newFeedback.neutral > 0 || newFeedback.bad > 0) {
-        setHasFeedback(true); 
-      }
-      return newFeedback;
-    });
+const hasFeedback = feedback.good > 0 || feedback.neutral > 0 || feedback.bad > 0;
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      
+        ...prevFeedback,
+        [feedbackType]: prevFeedback[feedbackType] + 1,
+      }));
   };
 
-    const resetFeedback = () => {
+
+  const resetFeedback = () => {
     setFeedback({
       good: 0,
       neutral: 0,
       bad: 0,
     });
-    setHasFeedback(false);
+    
+    localStorage.removeItem("feedback");
   };
-    const totalVotes = feedback.good + feedback.neutral + feedback.bad;
- 
- 
+  const totalVotes = feedback.good + feedback.neutral + feedback.bad;
 
-
- const positiveFeedback =  totalVotes
+  const positiveFeedback = totalVotes
     ? Math.round((feedback.good / totalVotes) * 100)
-    : 0; 
+    : 0;
 
   return (
     <div className="app-container">
       <Description />
-      <Options onClick={updateFeedback} total={totalVotes} reset={resetFeedback}/>
-      {hasFeedback && totalVotes > 0 ?(
-         <Feedback
-        good={feedback.good}
-        neutral={feedback.neutral}
-        bad={feedback.bad}
+      <Options
+        onClick={updateFeedback}
         total={totalVotes}
-        positive={positiveFeedback}
-        
-      />) : ( <Notification />)
-        
-      }
+        reset={resetFeedback}
+      />
+      {hasFeedback && totalVotes > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalVotes}
+          positive={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
